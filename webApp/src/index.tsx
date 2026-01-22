7f911614e62e74948a728c9a85d544815092db70
Falco Berendhaus <falcoberendhaus@gmail.com>
2026-01-22 17:16:14 +0100

    clear tsc

--- webApp/src/index.tsx
+++ webApp/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from "./App.tsx";
 import {BrowserRouter} from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
-import "./index.css";
+import "./Index.css";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
